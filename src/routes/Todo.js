8d948ffc07bce2873b6b3a9f0b55fdbf17f4d82e
Wu Ming-Chiao <mingchiao@Wus-MacBook-Pro.local>
2020-11-08 11:56:52 +0800

    fetch todo list

--- src/routes/Todo.js
+++ src/routes/Todo.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API from '../api';
 
 export default function Todo() {
+    const [list, setList] = useState([]);
+    useEffect(() => {
+        API.get('/todo')
+        .then(res => setList(res.data));
+    }, []);
     return (
-        <div>todo</div>
+        <div>
+        {list.map(todo => (
+            <div key={todo.id}>
+                {todo.content}
+            </div>
+        ))
+        }
+        </div>
     );
 }
